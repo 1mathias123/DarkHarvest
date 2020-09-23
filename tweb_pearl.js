e53e432938fab6c8f5043b9817c56da54f49a63e
1mathias123 <45027719+1mathias123@users.noreply.github.com>
2020-09-23 19:36:14 +0200

    Update tweb_pearl.js

--- tweb_pearl.js
+++ tweb_pearl.js
@@ -3,7 +3,7 @@ const tokenAddress = 'TJCX1n9xGZAh4tJWUapshqWTQvaiq9yC3z' // this is the lp toke
 
 const dhmAddress = 'TGAuyPkfBYfA8ZVEQqboaNK1iStJokgnnW' //dmh
 
-const stakingAddress = 'TBULrT9YTUPCsQpjWZTqEb5VStbuwBmRgb'// staking
+const stakingAddress = 'TV7LQxJFJrA8BcaKL3YYuGQmpfRnVq5nDt'// staking
 
 let tokenInstance = undefined
 let dhmInstance = undefined
