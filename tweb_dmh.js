78fb14caaa9eacec9bfb5a1a028dcc8b09e21dd1
1mathias123 <45027719+1mathias123@users.noreply.github.com>
2020-09-25 00:05:07 +0200

    Update tweb_dmh.js

--- tweb_dmh.js
+++ tweb_dmh.js
@@ -3,7 +3,7 @@ const tokenAddress = 'TJCX1n9xGZAh4tJWUapshqWTQvaiq9yC3z' // this is the lp toke
 
 const dhmAddress = 'TGAuyPkfBYfA8ZVEQqboaNK1iStJokgnnW' //dmh
 
-const stakingAddress = 'TPGJ1qof4bNrQbtnJ1eXbwJQ4tUcqfyQ3E'// staking
+const stakingAddress = 'TDf1RjaAY3ptvZiFWrZPb29oWqvhF1B35q'// staking
 
 let tokenInstance = undefined
 let dhmInstance = undefined
