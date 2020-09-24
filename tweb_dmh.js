1dc432de38ff13cd5dc45dec6ecdcf7dae402cd5
1mathias123 <45027719+1mathias123@users.noreply.github.com>
2020-09-24 23:54:56 +0200

    Update tweb_dmh.js

--- tweb_dmh.js
+++ tweb_dmh.js
@@ -64,7 +64,7 @@ async function updateRewards() {
 
 async function stake() {
   stakingInstance = await tronWeb.contract().at(stakingAddress)
-  const amount = document.getElementById('tokenAmount').value*100
+  const amount = document.getElementById('tokenAmount').value*1000000
   const options = {
         feeLimit:100000000,
         callValue:0,
