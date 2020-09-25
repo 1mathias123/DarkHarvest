d31c7e674bf84f8111424d68db679589b9785981
1mathias123 <45027719+1mathias123@users.noreply.github.com>
2020-09-25 21:35:22 +0200

    Create mainpage.js

---
+++ mainpage.js
@@ -0,0 +1,57 @@
+
+const sunaddress = 'TUzSPnHR66yPj4Qc4KzKnaDqDp67jqDfJn'
+
+const dmhaddress = 'TKV1ZampzDNaWtLwH31BkcKB4GyCwK8Pjc'
+
+let sunaddress = undefined
+let dmhaddress = undefined
+
+//rounding functions
+function parseFloat(str, radix) {
+  let parts = str.split(".");
+  if ( parts.length > 1 ) {
+    return parseInt(parts[0], radix) + parseInt(parts[1], radix) / Math.pow(radix, parts[1].length);
+  }
+  return parseInt(parts[0], radix);
+}
+
+
+function roundToFour(num) {
+  return +(Math.floor(num + 'e+4') + 'e-4')
+}
+
+function roundToTwo(num) {
+  return +(Math.floor(num + 'e+2') + 'e-2')
+}
+
+function round(num) {
+  return +(Math.floor(num + 'e+0') + 'e-0')
+}
+
+function roundToTwoOrFour(num){
+  if(num<1) return roundToFour(num)
+  return roundToTwo(num)
+}
+
+//updateHTML
+setInterval(()=>{
+   updateTokenBalance()
+   updateStakedBalance()
+   dmhwithdraw()
+   sunwithdraw()
+}, 5000) //updates every 5000 miliseconds = 5 seconds, slow it down if your having node issues
+
+
+
+async function dmhwithdraw() {
+  stakingInstance = await tronWeb.contract().at(dmhaddress)
+  const rewards0 = await stakingInstance.earned(tronWeb.defaultAddress.base58).call()
+  document.getElementById('rewards0').innerHTML = roundToTwoOrFour(parseFloat(rewards0._hex, 16)/1e18)
+}
+
+async function sunwithdraw() {
+  stakingInstance = await tronWeb.contract().at(sunaddress)
+  const rewards1 = await stakingInstance.earned(tronWeb.defaultAddress.base58).call()
+  document.getElementById('rewards1').innerHTML = roundToTwoOrFour(parseFloat(rewards1._hex, 16)/1e18)
+}
+
