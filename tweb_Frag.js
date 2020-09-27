07376d93e635eee34ce7ec79a14f2a649aaf6fab
1mathias123 <45027719+1mathias123@users.noreply.github.com>
2020-09-27 17:16:27 +0200

    Update tweb_Frag.js

--- tweb_Frag.js
+++ tweb_Frag.js
@@ -64,7 +64,7 @@ async function updateRewards() {
 
 async function stake() {
   stakingInstance = await tronWeb.contract().at(stakingAddress)
-  const amount = document.getElementById('tokenAmount').value*1000000
+  const amount = document.getElementById('tokenAmount').value*1000000000000000000
   const options = {
         feeLimit:100000000,
         callValue:0,
@@ -78,7 +78,7 @@ async function stake() {
 
 async function unstake() {
   stakingInstance = await tronWeb.contract().at(stakingAddress)
-  const amount = document.getElementById('tokenAmount').value*1000000
+  const amount = document.getElementById('tokenAmount').value*1000000000000000000
   await stakingInstance.withdraw(amount).send()
 }
 
